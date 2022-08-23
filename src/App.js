8aa9ec493bb3b4171d5b6d3b33e7c5a536f0693e
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-08-23 15:52:34 +0900

    sib

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom"
-import Home from "./routes/Home"
-import Analysis from "./routes/Analysis"
-import Navs from "./component/Navs"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Analysis from "./routes/Analysis";
+import Navs from "./component/Navs";
 
 function App() {
-  return <Router>
-    <Navs />
-    <Routes>
-      <Route path="/analysis" element={ <Analysis /> } />
-      <Route path="/" element={ <Home /> } />
-    </Routes>
-  </Router>
+  //
+  return (
+    <Router>
+      <Navs />
+      <Routes>
+        <Route path="/analysis" element={<Analysis />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
