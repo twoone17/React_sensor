9eeec50e82ea355fab91a1bb9457fa18490e88e4
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-08-23 17:30:40 +0900

    [ADD]Header 블루투스 연동

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,14 +1,40 @@
+import { useSearchParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function Header() {
-   
-    return (
-        <div>
-            <h1>X : 120</h1>
-            <h1>Y : 120</h1>
-        </div>
-        
+  const [connect, setConnect] = useState(true);
+  const [device, setDevice] = useState("");
 
-    )
+  const onClickBluetooth = () => {
+    navigator.bluetooth
+      .requestDevice({
+        acceptAllDevices: true,
+        optionalServices: ["battery_service"],
+      })
+      .then((device) => {
+        console.log(`Name: ${device}`);
+        console.log(device);
+        setConnect(false);
+        const interval = setInterval(() => {
+          setDevice(device);
+          console.log("refresh");
+          console.log(device);
+        }, 1000);
+      })
+      .catch((error) => console.error(`Something went wrong. ${error}`));
+  };
+  console.log(device);
 
+  return (
+    <div>
+      <button onClick={onClickBluetooth}>Click to connect bluetooth</button>
+      {connect ? (
+        <h1>Device Loading...</h1>
+      ) : (
+        <h1>Device name : {device.name}</h1>
+      )}
+    </div>
+  );
 }
 
 export default Header;
