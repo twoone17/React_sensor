8a6bbb908d9397f1fe1dc733dff538d430235179
JoSooha <dlrhdcjs@naver.com>
2022-08-30 23:56:52 +0900

    0830

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,29 +2,44 @@ import { useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import styles from "./Header.module.css";
 
+
 function Header() {
   const [connect, setConnect] = useState(true);
   const [device, setDevice] = useState("");
-
+  const ACCELEROMETER_SERVICE_UUID = "741c12b9-e13c-4992-8a5e-fce46dec0bff";
 
   const onClickBluetooth = () => {
     navigator.bluetooth
       .requestDevice({
         acceptAllDevices: true,
-        optionalServices: ["battery_service"],
+        optionalServices: ["baad41b2-f12e-4322-9ba6-22cd9ce09832"],
       })
       .then((device) => {
-        console.log(`Name: ${device}`);
-        console.log(device);
-        setConnect(false);
-        const interval = setInterval(() => {
-          setDevice(device);
-          console.log("refresh");
-          console.log(device);
-        }, 1000);
+        console.log("Connecting to GATT Server...");
+        return device.gatt.connect();
       })
-      .catch((error) => console.error(`Something went wrong. ${error}`));
+      .then((server) => {
+        console.log("Getting Battery Service...");
+        console.log(server);
+        return server.getPrimaryService("baad41b2-f12e-4322-9ba6-22cd9ce09832");
+      })
+      .then((service) => {
+        console.log("Getting Battery Level Characteristic...");
+        console.log(service.getCharacteristic());
+        return service.getCharacteristic();
+      })
+      // .then((characteristic) => {
+      //   let batteryLevel = value.getUint8(0);
+      // })
+      .then((value) => {
+        let batteryLevel = value.getUint8(0);
+        console.log("> Battery Level is " + batteryLevel + "%");
+      })
+      .catch((error) => {
+        console.log("Argh! " + error);
+      });
   };
+
   console.log(device);
 
   return (
@@ -35,6 +50,10 @@ function Header() {
       ) : (
         <h1>Device name : {device.name}</h1>
       )}
+      <p></p>
+      <h1>hu</h1>
+      
+
     </div>
   );
 }
