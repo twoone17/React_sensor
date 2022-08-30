2514e6686f55ce71885b5f612a375fdc49cb167e
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-08-31 00:32:37 +0900

    충돌나지마요

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,18 +1,13 @@
 import { useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import styles from "./Header.module.css";
-
-
 function Header() {
   const [connect, setConnect] = useState(true);
   const [device, setDevice] = useState("");
-  const ACCELEROMETER_SERVICE_UUID = "741c12b9-e13c-4992-8a5e-fce46dec0bff";
-
   const onClickBluetooth = () => {
     navigator.bluetooth
       .requestDevice({
         acceptAllDevices: true,
-        optionalServices: ["baad41b2-f12e-4322-9ba6-22cd9ce09832"],
+        optionalServices: ["66df5109-edde-4f8a-a5e1-02e02a69cbd5"],
       })
       .then((device) => {
         console.log("Connecting to GATT Server...");
@@ -21,41 +16,51 @@ function Header() {
       .then((server) => {
         console.log("Getting Battery Service...");
         console.log(server);
-        return server.getPrimaryService("baad41b2-f12e-4322-9ba6-22cd9ce09832");
+        return server.getPrimaryService("66df5109-edde-4f8a-a5e1-02e02a69cbd5");
       })
       .then((service) => {
-        console.log("Getting Battery Level Characteristic...");
-        console.log(service.getCharacteristic());
-        return service.getCharacteristic();
+        console.log("Getting acc x ..");
+        console.log(
+          service.getCharacteristic("741c12b9-e13c-4992-8a5e-fce46dec0bff")
+        );
+        return service.getCharacteristic(
+          "741c12b9-e13c-4992-8a5e-fce46dec0bff"
+        );
+        //y
+        // return service.getCharacteristic(
+        //   "baad41b2-f12e-4322-9ba6-22cd9ce09832"
+        // );
       })
-      // .then((characteristic) => {
-      //   let batteryLevel = value.getUint8(0);
-      // })
       .then((value) => {
-        let batteryLevel = value.getUint8(0);
-        console.log("> Battery Level is " + batteryLevel + "%");
+        value.addEventListener(
+          "characteristicvaluechanged",
+          handleBatteryLevelChanged
+        );
+        return value.readValue();
+      })
+      .then((value) => {
+        console.log(value);
+        console.log(value.getUint8(0));
       })
       .catch((error) => {
         console.log("Argh! " + error);
       });
   };
 
+  function handleBatteryLevelChanged(event) {
+    console.log("changed!");
+  }
   console.log(device);
 
   return (
-    <div className={`${styles.row} ${styles.back}`}>
+    <div>
       <button onClick={onClickBluetooth}>Click to connect bluetooth</button>
       {connect ? (
         <h1>Device Loading...</h1>
       ) : (
         <h1>Device name : {device.name}</h1>
       )}
-      <p></p>
-      <h1>hu</h1>
-      
-
     </div>
   );
 }
-
 export default Header;
