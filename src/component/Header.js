90adf6986b3377554ae9c8880a05cd2f902e575a
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-08-31 00:13:42 +0900

    value값 받기

--- src/component/Header.js
+++ src/component/Header.js
@@ -9,20 +9,49 @@ function Header() {
     navigator.bluetooth
       .requestDevice({
         acceptAllDevices: true,
-        optionalServices: ["battery_service"],
+        optionalServices: ["66df5109-edde-4f8a-a5e1-02e02a69cbd5"],
       })
       .then((device) => {
-        console.log(`Name: ${device}`);
-        console.log(device);
-        setConnect(false);
-        const interval = setInterval(() => {
-          setDevice(device);
-          console.log("refresh");
-          console.log(device);
-        }, 1000);
+        console.log("Connecting to GATT Server...");
+        return device.gatt.connect();
       })
-      .catch((error) => console.error(`Something went wrong. ${error}`));
+      .then((server) => {
+        console.log("Getting Battery Service...");
+        console.log(server);
+        return server.getPrimaryService("66df5109-edde-4f8a-a5e1-02e02a69cbd5");
+      })
+      .then((service) => {
+        console.log("Getting acc x ..");
+        console.log(
+          service.getCharacteristic("741c12b9-e13c-4992-8a5e-fce46dec0bff")
+        );
+        return service.getCharacteristic(
+          "741c12b9-e13c-4992-8a5e-fce46dec0bff"
+        );
+        //y
+        // return service.getCharacteristic(
+        //   "baad41b2-f12e-4322-9ba6-22cd9ce09832"
+        // );
+      })
+      .then((value) => {
+        value.addEventListener(
+          "characteristicvaluechanged",
+          handleBatteryLevelChanged
+        );
+        return value.readValue();
+      })
+      .then((value) => {
+        console.log(value);
+        console.log(value.getUint8(0));
+      })
+      .catch((error) => {
+        console.log("Argh! " + error);
+      });
   };
+
+  function handleBatteryLevelChanged(event) {
+    console.log("changed!");
+  }
   console.log(device);
 
   return (
