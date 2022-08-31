65e883973e701f4dc13a6e4e7bd3d75bdbc4cd23
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-08-31 20:25:24 +0900

    [ADD] 실시간 angle 반영

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,64 +2,39 @@ import { useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import styles from "./Header.module.css";
 function Header() {
-  const [connect, setConnect] = useState(true);
+  const [connect, setConnect] = useState("0");
   const [device, setDevice] = useState("");
   const [Xangle, setXangle] = useState("");
   const [Yangle, setYangle] = useState("");
   const buffer = [];
-  const onClickBluetooth = () => {
-    navigator.bluetooth
-      .requestDevice({
+  const [startButton, setStartButton] = useState(false);
+  async function onClickBluetooth() {
+    try {
+      const deviceActivate = await navigator.bluetooth.requestDevice({
         acceptAllDevices: true,
         optionalServices: ["66df5109-edde-4f8a-a5e1-02e02a69cbd5"],
-      })
-      .then((device) => {
-        console.log("Connecting to GATT Server...");
-        setConnect(false);
-        setDevice(device);
-        return device.gatt.connect();
-      })
-      .then((server) => {
-        console.log("Getting Battery Service...");
-        console.log(server);
-        return server.getPrimaryService("66df5109-edde-4f8a-a5e1-02e02a69cbd5");
-      })
-      .then((service) => {
-        console.log("Getting acc x ..");
-        console.log(
-          service.getCharacteristic("741c12b9-e13c-4992-8a5e-fce46dec0bff")
-        );
-        return service.getCharacteristic(
-          "741c12b9-e13c-4992-8a5e-fce46dec0bff"
-        );
-        //y
-        // return service.getCharacteristic(
-        //   "baad41b2-f12e-4322-9ba6-22cd9ce09832"
-        // );
-      })
-      .then((value) => {
-        value.addEventListener(
-          "characteristicvaluechanged",
-          handleBatteryLevelChanged
-        );
-        return value.readValue();
-      })
-      .then((value) => {
-        console.log(value);
-        console.log(value.getUint8(0));
-        for (let i = 0; i < 7; i++) {
-          buffer[i] = value.getUint8(i).toString(16);
-        }
-        // buffer.toString();
-        const bufferMerge = buffer.join("");
-        const Xanglevalue = hex2a(bufferMerge);
-        setXangle(Xanglevalue);
-        console.log(Xanglevalue);
-      })
-      .catch((error) => {
-        console.log("Argh! " + error);
       });
-  };
+
+      console.log("Connecting to GATT Server...");
+      const server = await deviceActivate.gatt.connect();
+      const service = await server.getPrimaryService(
+        "66df5109-edde-4f8a-a5e1-02e02a69cbd5"
+      );
+      const characteristic = await service.getCharacteristic(
+        "741c12b9-e13c-4992-8a5e-fce46dec0bff"
+      );
+      characteristic.addEventListener(
+        "characteristicvaluechanged",
+        handleBatteryLevelChanged
+      );
+
+      setInterval(() => {
+        const Xvalue = characteristic.readValue();
+      }, 500);
+    } catch (error) {
+      console.log("Argh! " + error);
+    }
+  }
 
   function hex2a(hexx) {
     let hex = hexx.toString(); //force conversion
@@ -71,7 +46,16 @@ function Header() {
 
   function handleBatteryLevelChanged(event) {
     const gyroX = event.target.value.getUint8(0);
+    const length = event.target.value.byteLength;
     console.log("changed value: " + gyroX);
+    for (let i = 0; i < length; i++) {
+      buffer[i] = event.target.value.getUint8(i).toString(16);
+    }
+    // buffer.toString();
+    const bufferMerge = buffer.join("");
+    const Xanglevalue = hex2a(bufferMerge);
+    setXangle(Xanglevalue);
+    console.log(Xanglevalue);
   }
   console.log(device);
 
