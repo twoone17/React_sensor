681b5bbec6641037251919fb883fda78f4b40d25
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-08-31 20:32:01 +0900

    [ADD] sensor 값 실시간 화면에 띄우기

--- src/component/Header.js
+++ src/component/Header.js
@@ -17,19 +17,33 @@ function Header() {
 
       console.log("Connecting to GATT Server...");
       const server = await deviceActivate.gatt.connect();
+      //Service
       const service = await server.getPrimaryService(
         "66df5109-edde-4f8a-a5e1-02e02a69cbd5"
       );
+      //X sensor characteristic
       const characteristic = await service.getCharacteristic(
         "741c12b9-e13c-4992-8a5e-fce46dec0bff"
       );
+
+      //Y sensor characteristic
+      const characteristic2 = await service.getCharacteristic(
+        "baad41b2-f12e-4322-9ba6-22cd9ce09832"
+      );
+
       characteristic.addEventListener(
         "characteristicvaluechanged",
         handleBatteryLevelChanged
       );
 
+      characteristic2.addEventListener(
+        "characteristicvaluechanged",
+        handleBatteryLevelChanged2
+      );
+
       setInterval(() => {
         const Xvalue = characteristic.readValue();
+        const Yvalue = characteristic2.readValue();
       }, 500);
     } catch (error) {
       console.log("Argh! " + error);
@@ -56,6 +70,20 @@ function Header() {
     const Xanglevalue = hex2a(bufferMerge);
     setXangle(Xanglevalue);
     console.log(Xanglevalue);
+  }
+
+  function handleBatteryLevelChanged2(event) {
+    const gyroY = event.target.value.getUint8(0);
+    const length = event.target.value.byteLength;
+    console.log("changed value: " + gyroY);
+    for (let i = 0; i < length; i++) {
+      buffer[i] = event.target.value.getUint8(i).toString(16);
+    }
+    // buffer.toString();
+    const bufferMerge = buffer.join("");
+    const Yanglevalue = hex2a(bufferMerge);
+    setYangle(Yanglevalue);
+    console.log(Yanglevalue);
   }
   console.log(device);
 
