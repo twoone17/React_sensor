fb5379236d06c9181c131df259ae5bbd7281cc28
JoSooha <dlrhdcjs@naver.com>
2022-09-08 23:55:42 +0900

    0908 piechart 수정중

--- src/routes/Analysis.js
+++ src/routes/Analysis.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { PieChart } from 'react-minimal-pie-chart';
 
-const Analysis = ({ data }) => (
-  <PieChart
-    data={[
-      { title: "One", value: 10, color: "#E38627" },
-      { title: "Two", value: 15, color: "#C13C37" },
-      { title: "Three", value: 20, color: "#6A2135" },
-    ]}
-    label = {({dataEntry}) => dataEntry.value}
-    animate
-  />
-);
-
+function Analysis({ data }) {
+  return (
+    <div className="back">
+      <PieChart
+        data={[
+          { title: "One", value: 10, color: "#E38627" },
+          { title: "Two", value: 15, color: "#C13C37" },
+          { title: "Three", value: 20, color: "#6A2135" },
+        ]}
+        label={({ dataEntry }) => dataEntry.value}
+        animate
+        //width = 
+        //viewBoxSize = {[100, 80]}
+      />
+    </div>
+  );
+}
+  
 export default Analysis;
