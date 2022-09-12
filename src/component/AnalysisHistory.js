f6e67e33def6c5ce5c5a927df5f082959889ea99
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-09-12 15:38:17 +0900

    [ADD]로그 출력까지

--- src/component/AnalysisHistory.js
+++ src/component/AnalysisHistory.js
@@ -4,14 +4,27 @@ import { PieChart } from "react-minimal-pie-chart";
 function AnalysisHistory({ localStorageKey, localStorageValue, dateChange }) {
   const [state1, setState1] = useState("start");
   // const [localStoragekeyState, setlocalStorageKeyState] = useState([]);
-  let storageArray = [];
-  const [storageArraystate, SetstorageArraystate] = useState([]);
+  let storageArray = [""];
+  const [storageArraystate, SetstorageArraystate] = useState([""]);
+
+  function StorageHistory({ storageArraystate }) {
+    const rendering = () => {
+      const result = [];
+
+      for (let i = 0; i < storageArraystate[0].length; i++) {
+        result.push(<li key={i}>{storageArraystate[0][i]}</li>);
+      }
+      return result;
+    };
+
+    return <div>{rendering()}</div>;
+  }
 
   function ShowDateLog() {
     let NewDate = localStorageKey[0].substring(0, 8);
     let DateSet = new Set();
     const Newarray = [];
-    console.log(NewDate);
+    // console.log(NewDate);
     for (let i = 0; i < localStorageKey.length; i++) {
       DateSet.add(NewDate);
       NewDate = localStorageKey[i].substring(0, 8);
@@ -23,18 +36,16 @@ function AnalysisHistory({ localStorageKey, localStorageValue, dateChange }) {
       SetstorageArraystate([]);
       for (let i = 0; i < localStorageKey.length; i++) {
         if (localStorageKey[i].includes(date)) {
-          console.log(localStorageKey[i]);
-          // setlocalStorageKeyState([localStorageKey[i], localStoragekeyState]);
           storageArray.push(localStorageKey[i]);
         }
       }
-      console.log(storageArray);
       // console.log("Please" + localStoragekeyState);
       setState1(date);
       SetstorageArraystate((storageArraystate) => [
         ...storageArraystate,
         storageArray,
       ]);
+      console.log(storageArraystate);
     };
 
     return (
@@ -46,8 +57,8 @@ function AnalysisHistory({ localStorageKey, localStorageValue, dateChange }) {
             </li>
           ))}
         </ul>
-        {state1}의 날짜
-        <p>{storageArraystate}</p>
+        {state1}의 로그 확인
+        <StorageHistory storageArraystate={storageArraystate} />
       </div>
     );
   }
