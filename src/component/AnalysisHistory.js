3d854c1cafe05590ad6dd8c49c267fde02cd0576
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-09-13 16:03:43 +0900

    충돌나지마용

--- src/component/AnalysisHistory.js
+++ src/component/AnalysisHistory.js
@@ -32,9 +32,6 @@ function AnalysisHistory({ localStorageKey, localStorageValue, dateChange }) {
       PieArray.push(PieKey, PieValue);
       console.log("PieKey " + PieArray[0]);
       console.log("PieValue" + PieArray[1]);
-      // console.log(PieChartState1);
-      // console.log("PieChartState[0]" + PieChartState1[0]);
-      // console.log("PieChartState[1]" + PieChartState1[1]);
       setLiState(i);
     };
     const ShowVibration = (date, i) => {
@@ -127,9 +124,6 @@ function AnalysisHistory({ localStorageKey, localStorageValue, dateChange }) {
     <div>
       기록 로그
       <ShowDateLog />
-      {/* <h1>{localStorageKey}</h1>
-      <h1>{localStorageValue}</h1>
-      <h1>{dateChange}</h1> */}
     </div>
   );
 }
